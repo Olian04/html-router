4eee7d6a271c9056192a31b31339bca7c646c0de
Oliver <rift95@live.se>
2019-03-06 16:57:01 +0100

    implemented router for real, finaly...

--- README.md
+++ README.md
@@ -2,23 +2,22 @@
 
 A router designed with simplicity in mind where you dont have to touch any javascript if you dont want to.
 
-__Note:__ The name is a work in progress.
-
 ```html
 <body>
   <router-view></router-view>
-  
-  <router-route path="/home">
-    <h1>Home route!</h1>
-    <button onclick="routerRedirect('/login')">Login</button>
-  </router-route>
-  
+
   <router-route path="/" redirect="/home"></router-route>
-  
-  <router-route path="/login">
-    <h1>Login route!</h1>
+
+  <router-route path="/home">
+      <h1>Home!</h1>
+      <button onclick="router.push('/login')">Load Login!</button>
   </router-route>
+
+  <router-route path="/login">
+      <h1>Login!</h1>
+      <button onclick="router.push('/home')">Load Home!</button>
+  </router-route>
+
+  <script src="./web-component-router.js"></script>
 </body>
 ```
-
-__WIP:__ https://jsfiddle.net/t7fq8ejc/9/
---
+++ web-component-router.js
@@ -0,0 +1,83 @@
+window.router = (() => {
+    const R = {
+        push(path) {
+              window.location.hash = '#'+path;
+        },
+        __routes: {},
+        __routerView: null
+    };
+
+    const loadPath = path => {
+      if (path in R.__routes) {
+        if ('redirect' in R.__routes[path]) {
+            R.push(R.__routes[path].redirect);
+        } else {
+          R.__routerView.innerHTML = '';
+          R.__routes[path].element.hidden = false;
+          R.__routerView.append(R.__routes[path].element);
+        }
+      } else {
+          console.warn('router: Attempting to load unknown path "'+ path +'"');
+      }
+    }
+    
+    window.addEventListener('hashchange', () => {
+        loadPath(window.location.hash.substr(1));
+    });
+    
+    const currentHash = window.location.hash;
+    if (currentHash === '') {
+      // Redirect to index page theres no hash
+      window.location.hash = '#/';
+    } else {
+        window.location.hash = '';
+        window.location.hash = currentHash;
+    }
+    
+    window.customElements.define('router-view', 
+      class extends HTMLElement {
+          connectedCallback() {
+              if (R.__routerView) {
+                throw new Error('router-view: There can only be one router-view.');
+            }
+            R.__routerView = this;
+          }
+      }
+    );
+    
+    window.customElements.define('router-route', 
+      class extends HTMLElement {
+          constructor(...args) {
+            super(...args);
+               this.hidden = true;
+          }
+    
+          connectedCallback() {
+              this.prepareAttributes();
+            
+            const route = {
+                path: this.getAttribute('path')
+            };
+            
+            if (this.hasAttribute('redirect')) {
+                route.redirect = this.getAttribute('redirect');
+            } else {
+                route.element = this;
+            }
+            
+            R.__routes[route.path] = route;
+          }
+          
+          prepareAttributes() {
+            if (!this.hasAttribute('path')) {
+              throw Error('router-route: Missing required attribute "path".');
+            }
+            if (this.hasAttribute('redirect') && this.firstChild !== null) {
+                throw Error('router-route: Redirect route may not have children');
+            }
+         }
+      }
+    );
+
+    return R;
+})();
